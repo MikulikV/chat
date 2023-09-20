c558d3b2a287b32bc78397e8384111f8a4571a2a
MikulikV <mikulik1995@outlook.com>
2023-09-20 16:18:40 -0400

    Create backend

---
+++ backend/server.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, Response, stream_with_context, json
+from flask_cors import CORS
+import requests
+import sseclient
+import os
+
+from dotenv import load_dotenv 
+
+# OPENAI_API_KEY
+load_dotenv()
+OPENAI_API_KEY = os.getenv('AZURE_OPENAI_API_KEY')
+# to deploy in Azure
+# OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+app = Flask(__name__)
+
+# handle cors
+CORS(app)
+
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
+@app.route('/api/prompt', methods=['GET', 'POST'])
+def prompt():
+    if request.method == 'POST':
+        prompt = request.json['prompt']
+    
+        def generate():
+            url = 'https://api.openai.com/v1/chat/completions'
+            headers = {
+                'content-type': 'application/json; charset=utf-8',
+                'Authorization': f"Bearer {OPENAI_API_KEY}"            
+            }
+
+            data = {
+                'model': 'gpt-3.5-turbo',
+                'messages': [
+                    {'role': 'system', 'content': 'You are an AI assistant that answers questions about anything.'},
+                    {'role': 'user', 'content': prompt}
+                ],
+                'temperature': 0, 
+                'max_tokens': 1000,
+                'stream': True,            
+            }
+
+            response = requests.post(url, headers=headers, data=json.dumps(data), stream=True)
+            client = sseclient.SSEClient(response)
+            for event in client.events():
+                if event.data != '[DONE]':
+                    try:
+                        text = json.loads(event.data)['choices'][0]['delta']['content']
+                        yield(text)
+                    except:
+                        yield('')
+
+        return Response(stream_with_context(generate()))
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
