0cba07a9a7dff821d0891dd0526325af0d0decf5
MikulikV <mikulik1995@outlook.com>
2023-09-21 14:06:13 -0400

    Add function to count tokens

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import requests
 import sseclient
 import os
+import tiktoken
 # from openai.error import RateLimitError
 
 from dotenv import load_dotenv 
@@ -23,22 +24,23 @@ CORS(app)
 def index():
     return "Hello world"
 
-
+# Constants
 max_response_tokens = 500
 token_limit= 4000
 prompt = "You are a helpful assistant, your name is CBN Assistant. You are a Christian and your task is to help people to be better."
 
-# def num_tokens_from_messages(messages, model="gpt-3.5-turbo"):
-#     encoding = tiktoken.encoding_for_model(model)
-#     num_tokens = 0
-#     for message in messages:
-#         num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
-#         for key, value in message.items():
-#             num_tokens += len(encoding.encode(value))
-#             if key == "name":  # if there's a name, the role is omitted
-#                 num_tokens += -1  # role is always required and always 1 token
-#     num_tokens += 2  # every reply is primed with <im_start>assistant
-#     return num_tokens
+# Function to count tokens
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo"):
+    encoding = tiktoken.encoding_for_model(model)
+    num_tokens = 0
+    for message in messages:
+        num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":  # if there's a name, the role is omitted
+                num_tokens += -1  # role is always required and always 1 token
+    num_tokens += 2  # every reply is primed with <im_start>assistant
+    return num_tokens
 
 
 @app.route('/api/chat', methods=['GET', 'POST'])
@@ -54,10 +56,11 @@ def chat():
 
         conversation.append({"role": "user", "content": user_input})
 
-        # num_tokens = num_tokens_from_messages(conversation)
-        # while (num_tokens+max_response_tokens >= token_limit):
-        #     del conversation[1] 
-        #     num_tokens = num_tokens_from_messages(conversation)
+        # Delete messages from memory to avoid model's token limit
+        num_tokens = num_tokens_from_messages(conversation)
+        while (num_tokens+max_response_tokens >= token_limit):
+            del conversation[1] 
+            num_tokens = num_tokens_from_messages(conversation)
 
         # try:
         def generate():
